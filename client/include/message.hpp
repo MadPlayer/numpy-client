7001aee60b8c91c4ffb7de4805501649cfd0ade9
kch32051 <kch32051@etri.re.kr>
2022-01-27 17:07:50 +0900

    initial version message

---
+++ client/include/message.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "asio.hpp"
+
+namespace divided_client
+{
+  class post_message
+  {
+    std::string header_;
+    asio::streambuf buffer_;
+    std::iostream data_stream_;
+    bool invalied_;
+
+  public:
+    post_message(std::string &dir);
+    void send(asio::ip::tcp::socket &s) throw();
+    std::size_t receive(asio::ip::tcp::socket &s) throw();
+    std::iostream & get_stream();
+  };
+
+}
---
+++ client/src/message.cpp
@@ -0,0 +1,58 @@
+#include "message.hpp"
+#include "asio/error.hpp"
+#include "asio/execution_context.hpp"
+
+using namespace asio;
+using namespace asio::ip;
+using namespace divided_client;
+
+  // post_message();
+  // post_message(std::string &dir);
+  // std::size_t append(std::string &&data);
+  // void send(tcp::socket &s) throw();
+  // std::size_t receive(tcp::socket &s) throw();
+  // std::iostream & get_stream();
+
+post_message::post_message(std::string &dir):
+  data_stream_(buffer_)
+{
+  header_ = "POST " + dir + "HTTP/1.0\r\n"
+    "Content-Type: application/octet-stream\r\n"
+    "Content-Length: ";
+
+  // message has no data yet
+  invalied_ = true;
+}
+
+void post_message::send(tcp::socket &s) throw()
+{
+  header_ += std::to_string(buffer_.size()) + "\r\n\r\n";
+
+  write(s, buffer(header_));
+  write(s, buffer_);
+  invalid_ = true;
+}
+
+std::size_t
+post_message::receive(tcp::socket &s) throw()
+{
+  std::string header ;
+  error_code ec;
+
+  read(s, buffer_, ec);
+
+  // get rid of HTTP header
+  size_t bytes = 0;
+  while (bytes > 1)
+    {
+      std::getline(data_stream_, header);
+      bytes = header.length();
+    }
+  invalid_ = false;
+}
+
+std::iostream &
+post_message::get_stream()
+{
+  return data_stream_;
+}

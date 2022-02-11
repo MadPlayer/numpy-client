fb18c17add9d621aa52e5ec685d9ff978d41b8b0
kch32051 <kch32051@etri.re.kr>
2022-02-11 09:55:48 +0900

    better onnx string (remove mem leak)

--- client/include/model.hpp
+++ client/include/model.hpp
@@ -2,8 +2,47 @@
 
 #include "onnxruntime_cxx_api.h"
 #include "opencv2/core/mat.hpp"
+#include "blob.hpp"
+#include "tensor.hpp"
 #include <vector>
 #include <numeric>
+
+class onnx_string
+{
+  const char *c_str_ = nullptr;
+  Ort::AllocatorWithDefaultOptions allocator_;
+public:
+  onnx_string()
+  {
+  }
+
+  onnx_string(char *c_str, Ort::AllocatorWithDefaultOptions& allocator):
+    c_str_(c_str), allocator_(allocator)
+  {
+  }
+
+  void operator = (onnx_string&& os)
+  {
+    if (c_str_)
+      {
+        allocator_.Free(reinterpret_cast<void*>(const_cast<char *>(c_str_)));
+        c_str_ = nullptr;
+      }
+    c_str_ = os.c_str();
+    os.c_str_ = nullptr;
+  }
+
+  ~onnx_string()
+  {
+    if (c_str_)
+      allocator_.Free(reinterpret_cast<void*>(const_cast<char *>(c_str_)));
+  }
+
+  const char * c_str() const
+  {
+    return c_str_;
+  }
+};
 
 class model
 {
@@ -16,6 +55,8 @@ public:
   // very dangerous (can dereference invalied memory)
   std::vector<float> & inference(cv::Mat& preprocessed);
 
+  void inference(cv::Mat& preprocessed, body::tensor& output);
+
 private:
   Ort::Session session_;
   Ort::Value input_tensor_{nullptr};
@@ -23,6 +64,8 @@ private:
   int64_t input_size_;
   std::vector<int64_t> input_shape_;
   std::vector<int64_t> output_shape_;
+  onnx_string input_name_;
+  onnx_string output_name_;
   Ort::MemoryInfo allocator_info_;
   std::vector<float> result_;
 };
--- client/src/model.cpp
+++ client/src/model.cpp
@@ -1,12 +1,15 @@
 #include "model.hpp"
 
-
 model::model(Ort::Env& env, const char *model_path, Ort::SessionOptions &option):
   session_(env, model_path, option),
   allocator_info_(Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault))
 {
   input_shape_ = session_.GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
   output_shape_ = session_.GetOutputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+
+  Ort::AllocatorWithDefaultOptions allocator;
+  input_name_ = { session_.GetInputName(0, allocator), allocator };
+  output_name_ = { session_.GetOutputName(0, allocator), allocator };
 
   input_size_ = std::accumulate(input_shape_.begin(), input_shape_.end(), 1, std::multiplies<int64_t>{});
   auto output_size = std::accumulate(output_shape_.begin(), output_shape_.end(), 1, std::multiplies<int64_t>{});
@@ -21,18 +24,12 @@ model::model(Ort::Env& env, const char *model_path, Ort::SessionOptions &option)
 std::vector<float> &
 model::inference(std::vector<float> &input_data)
 {
-  Ort::AllocatorWithDefaultOptions allocator;
-  const char *input_name = session_.GetInputName(0, allocator);
-  const char *output_name = session_.GetOutputName(0, allocator);
-  const char *input_names[] = {input_name};
-  const char *output_names[] = {output_name};
+  const char *input_names[] = {input_name_.c_str()};
+  const char *output_names[] = {output_name_.c_str()};
 
   input_tensor_ = Ort::Value::CreateTensor<float>(allocator_info_, input_data.data(), input_size_,
                                                   input_shape_.data(), input_shape_.size());
   session_.Run(Ort::RunOptions{nullptr}, input_names, &input_tensor_, 1, output_names, &output_tensor_, 1);
-
-  allocator.Free((void *)input_name);
-  allocator.Free((void *)output_name);
 
   return result_;
 }
@@ -41,18 +38,17 @@ model::inference(std::vector<float> &input_data)
 std::vector<float> &
 model::inference(cv::Mat& preprocessed)
 {
-  Ort::AllocatorWithDefaultOptions allocator;
-  const char *input_name = session_.GetInputName(0, allocator);
-  const char *output_name = session_.GetOutputName(0, allocator);
-  const char *input_names[] = {input_name};
-  const char *output_names[] = {output_name};
+  const char *input_names[] = {input_name_.c_str()};
+  const char *output_names[] = {output_name_.c_str()};
 
   input_tensor_ = Ort::Value::CreateTensor<float>(allocator_info_, reinterpret_cast<float *>(preprocessed.data), input_size_,
                                                   input_shape_.data(), input_shape_.size());
   session_.Run(Ort::RunOptions{nullptr}, input_names, &input_tensor_, 1, output_names, &output_tensor_, 1);
 
-  allocator.Free((void *)input_name);
-  allocator.Free((void *)output_name);
-
   return result_;
+}
+
+void inference(cv::Mat& preprocessed, body::tensor& output)
+{
+  
 }

9bac906ffbf6f1ec1b0c03a0cfc0886e45f1505e
kch32051 <kch32051@etri.re.kr>
2022-02-23 11:09:58 +0900

    update test server

--- test_numpy_server.py
+++ test_numpy_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import pickle
 import numpy as np
 import json
-from build.include.packet_pb2 import tensor
+from build.include.packet_pb2 import tensor, batched_tensor
 
 
 app = Flask(__name__)
@@ -12,14 +12,35 @@ def parse_tensor_from_string(data):
     t = tensor()
     t.ParseFromString(data)
     array = np.array(t.data)
-    array.shape=(t.channel, t.height, t.width)
+    array.shape = t.shape
     return array
 
 
 def serialize_numpy_to_tensor(numpy_array):
     t = tensor()
-    t.channel, t.height, t.width = numpy_array.shape
+    t.shape = list(numpy_array.shape)
     t.data.extend(numpy_array.flatten("A"))
+    return t.SerializeToString()
+
+
+def parse_tensors_from_string(data):
+    t = batched_tensor()
+    t.ParseFromString(data)
+    batch = []
+    for tensor in t.tensors:
+        array = np.array(tensor.data)
+        array.shape = tensor.shape
+        batch.append(array)
+    return np.array(batch)
+
+
+def serialize_numpy_to_tensors(numpy_array):
+    bt = batched_tensor()
+    for tensor in numpy_array:
+        t = tensor()
+        t.shape = list(tensor.shape)
+        t.data.extend(numpy_array.flatten("A"))
+        bt.tensors.append(t)
     return t.SerializeToString()
 
 
@@ -29,7 +50,7 @@ def home():
 
 
 @app.route("/numpy/inference", methods=["POST"])
-def get_msg():
+def processing_tensor():
     msg = request.data
     received = parse_tensor_from_string(msg)
     print(received.shape)
@@ -37,6 +58,10 @@ def get_msg():
     numpy_array = np.array([i for i in range(27)])
     numpy_array.shape = (3, 1, 9)
     return serialize_numpy_to_tensor(numpy_array)
+
+@app.route("/numpy/inference/batched", methods=["POST"])
+def processing_tensors():
+    pass
 
 
 if __name__ == '__main__':
